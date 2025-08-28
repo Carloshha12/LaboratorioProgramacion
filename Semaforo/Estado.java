f6e42f039129c5b60d957a032540129b0b4b7861
Juan Cruz <juancruz.oliva@est.fi.uncoma.edu.ar>
2025-08-28 18:01:18 -0300

    Update Estado.java

--- Semaforo/Estado.java
+++ Semaforo/Estado.java
@@ -1,7 +1,7 @@
 public abstract class Estado {
     private final Semaforo semaforo;
 
-    public int tiempo; // segundos
+    private int tiempo; // segundos
     public Semaforo getSemaforo() { return semaforo; }
 
     public Estado(Semaforo semaforo) { this.semaforo = semaforo; }
